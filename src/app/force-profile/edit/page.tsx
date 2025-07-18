271312d0d3e3d056ceb4c36aaba1d4f97cb23e46
LDFuentesAscanio <ludafuasca@gmail.com>
2025-07-18 18:14:06 -0300

    implementacion profileCustomerForm

--- src/app/force-profile/edit/page.tsx
+++ src/app/force-profile/edit/page.tsx
@@ -1,26 +1,53 @@
 'use client';
-
-import { Suspense } from 'react';
+import { useEffect, useState } from 'react';
+import { supabase } from '@/utils/supabase/client';
 import ProfileForm from '@/components/organisms/ProfileForm/ProfileForm';
-import Image from 'next/image';
+import CustomerProfileForm from '@/components/organisms/ProfileForm/CustomerProfileForm';
 
-export default function ProfileEditPage() {
-  return (
-    <main className="min-h-screen flex items-center justify-center bg-prim px-4 text-white">
-      <Suspense
-        fallback={
-          <Image
-            src="/icons/carga.svg"
-            alt="Loading"
-            width={160}
-            height={160}
-            className="animate-pulse w-auto h-auto"
-            priority
-          />
-        }
-      >
-        <ProfileForm />
-      </Suspense>
-    </main>
-  );
+export default function ForceProfilePage() {
+  const [role, setRole] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (!user) return;
+
+      const { data, error } = await supabase
+        .from('users')
+        .select('role_id')
+        .eq('id', user.id)
+        .single();
+
+      if (error) {
+        console.error('Error fetching role:', error);
+        return;
+      }
+
+      const { data: roleData } = await supabase
+        .from('user_role')
+        .select('name')
+        .eq('id', data.role_id)
+        .single();
+
+      setRole(roleData?.name);
+      setLoading(false);
+    };
+
+    fetchRole();
+  }, []);
+
+  if (loading) return <p className="text-white">Loading...</p>;
+
+  if (role === 'Expert') {
+    return <ProfileForm />;
+  }
+
+  if (role === 'Customer') {
+    return <CustomerProfileForm />;
+  }
+
+  return <p className="text-white">Unauthorized</p>;
 }
