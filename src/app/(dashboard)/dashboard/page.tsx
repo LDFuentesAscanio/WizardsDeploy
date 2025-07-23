1578822a8b4e0a3bd35747df253917d08b607b68
LDFuentesAscanio <ludafuasca@gmail.com>
2025-07-23 10:00:10 -0300

    dashboard/page:Implementacion del CustomerDahsboarView

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,27 +1,35 @@
 'use client';
 
-import { Suspense } from 'react';
-import DashboardView from '@/components/views/DashboardView';
 import DashboardGuard from '@/components/guards/DashboardGuard';
+import CustomerDashboardView from '@/components/views/CustomerDashboardView';
+import DashboardView from '@/components/views/DashboardView';
 import Image from 'next/image';
+import { useDashboardUser } from '@/hooks/useDashboardUser';
 
 export default function DashboardPage() {
+  const { userData, loading } = useDashboardUser();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center py-10">
+        <Image
+          src="/icons/carga.svg"
+          alt="Loading"
+          width={160}
+          height={160}
+          className="animate-pulse w-auto h-auto"
+        />
+      </div>
+    );
+  }
+
+  if (!userData) return null;
+
+  const role = userData.role_name.toLowerCase();
+
   return (
     <DashboardGuard>
-      <Suspense
-        fallback={
-          <Image
-            src="/icons/carga.svg"
-            alt="Loading"
-            width={160}
-            height={160}
-            className="animate-pulse w-auto h-auto"
-            priority
-          />
-        }
-      >
-        <DashboardView />
-      </Suspense>
+      {role === 'customer' ? <CustomerDashboardView /> : <DashboardView />}
     </DashboardGuard>
   );
 }
